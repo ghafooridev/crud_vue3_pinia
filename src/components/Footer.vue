090c9079e6a473183cb20cdaa8c7ecfb1f35ccf8
ali.ghafouri <ghafooridev@gmail.com>
2024-05-01 16:48:23 +0330

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer
+    class="footer footer-center p-4 bg-primary text-base-content text-white"
+  >
+    <div>
+      <p>
+        Copyright © 2023 - All right reserved by Ghafooridev
+      </p>
+    </div>
+  </footer>
+</template>
